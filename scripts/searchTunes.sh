8c3c8dd7e5d945abd7fa8722c8dfd7c16efb8a85
Tung Nguyen <tung18tht@gmail.com>
2016-06-13 11:39:59 +0700

    Update shell script for ubuntu

--- scripts/searchTunes.sh
+++ scripts/searchTunes.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-for type in $2..$*
-do
-    find $1 -iname "*.$type" -exec stat -f "%N%n%z%n%Sm" {} +
-done
+# $1: directory path
+# $2..$*: file type
+
+if [[ uname=="Darwin" ]]; then # OS X
+    for type in $2..$*
+    do
+        find $1 -iname "*.$type" -exec stat -f "%N%n%z%n%Sm" {} +
+    done
+elif [[ uname=="Linux" ]]; then # Ubuntu
+    for type in $2..$*
+    do
+        find $1 -iname "*.$type" -printf "%p\n%s\n%t\n"
+    done
+fi
