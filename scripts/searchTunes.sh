859f9f0fc007a7914dcf5c09355a6fc2a6dba74c
Tung Nguyen <tung18tht@gmail.com>
2016-06-15 16:14:39 +0700

    Fix shell for path with spaces

--- scripts/searchTunes.sh
+++ scripts/searchTunes.sh
@@ -11,11 +11,11 @@
 if [[ uname=="Darwin" ]]; then # OS X
     for type in $2..$*
     do
-        find $1 -iname "*.$type" -exec stat -f "%N%n%z%n%Sm" {} +
+        find "$1" -iname "*.$type" -exec stat -f "%N%n%z%n%Sm" {} +
     done
 elif [[ uname=="Linux" ]]; then # Ubuntu
     for type in $2..$*
     do
-        find $1 -iname "*.$type" -printf "%p\n%s\n%t\n"
+        find "$1" -iname "*.$type" -printf "%p\n%s\n%t\n"
     done
 fi
