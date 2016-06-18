b45369c2840d0a7b0edb38acb6a690bbd82a06ed
Tung Nguyen <tung18tht@gmail.com>
2016-06-18 09:32:24 +0700

    Update sort script, inverse and string parameter

---
+++ scripts/sort.sh
@@ -0,0 +1,12 @@
+# $1: inverse variable
+# $2: string to sort
+
+case "$1" in
+    0)     # ascending
+        echo "$2" | sort
+    ;;
+
+    1)     # descending
+        echo "$2" | sort -r
+    ;;
+esac
