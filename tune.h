5287df416f496db81a5112885a8fcc0c4fe3bfbc
Tung Nguyen <tung18tht@gmail.com>
2016-06-19 20:19:24 +0700

    Done tune.h

---
+++ tune.h
@@ -0,0 +1,39 @@
+#ifndef TUNE_H
+#define TUNE_H
+
+#include <QObject>
+
+// Abstract Tune class with basic properties, get set methods
+// Names are taken from file paths
+
+class Tune : public QObject {
+    Q_OBJECT
+
+    Q_PROPERTY(QString name READ name)
+    Q_PROPERTY(QString path READ path)
+    Q_PROPERTY(QString size READ size)
+    Q_PROPERTY(QString lastModified READ lastModified)
+    Q_PROPERTY(bool duplicate READ duplicate WRITE setDuplicate)
+
+public:
+    explicit Tune(QObject *parent = 0);
+    Tune(QString path, QString size, QString lastModified);
+
+    QString name();
+    QString path();
+    QString size();
+    QString lastModified();
+    bool duplicate();
+
+    void setDuplicate(bool duplicate);
+
+private:
+    QString qName, qPath, qLastModified, qSize;
+    bool qDuplicate;
+    int iSize, iLastModified; // size, lastModified in int for sorting
+
+    QString evaluateSize(string size);
+    void setNameFromPath();
+};
+
+#endif // TUNE_H
